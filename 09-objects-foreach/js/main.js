f75f8a21d1ec7e1bc3fe03154f57d9060e77b420
Fernanda Palacios <fernandapalacios.dev@gmail.com>
2021-09-14 20:53:24 -0500

    [UPDATE] exercise forEach

--- 09-objects-foreach/js/main.js
+++ 09-objects-foreach/js/main.js
@@ -136,53 +136,53 @@ arrayMentor = [
 */
 
 
-const getArrayWithObject = (arrayMentors) => {
-    let newArrayMentors = []
-    for (let i = 0; i < arrayMentors.length; i++) {
-        // console.log(arrayMentors[i])
-        let name = arrayMentors[i][0]
-        let lastName = arrayMentors[i][1]
-        let mentorObject = { name, lastName }
-        newArrayMentors.push(mentorObject)
-    }
-    return newArrayMentors
-}
+// const getArrayWithObject = (arrayMentors) => {
+//     let newArrayMentors = []
+//     for (let i = 0; i < arrayMentors.length; i++) {
+//         // console.log(arrayMentors[i])
+//         let name = arrayMentors[i][0]
+//         let lastName = arrayMentors[i][1]
+//         let mentorObject = { name, lastName }
+//         newArrayMentors.push(mentorObject)
+//     }
+//     return newArrayMentors
+// }
 
-let otherArrayMentors = getArrayWithObject(dataArray)
+// let otherArrayMentors = getArrayWithObject(dataArray)
 
-// console.log(otherArrayMentors)
+// // console.log(otherArrayMentors)
 
-let namesArray = [
-    "Elias",
-    "Pao",
-    "Pablo",
-    "Nancy"
-]
+// let namesArray = [
+//     "Elias",
+//     "Pao",
+//     "Pablo",
+//     "Nancy"
+// ]
 
-namesArray.forEach((name, index) => {
-    console.log(`el nombre es: ${name} en la posicion ${index}`)
-})
+// namesArray.forEach((name, index) => {
+//     console.log(`el nombre es: ${name} en la posicion ${index}`)
+// })
 
-let arrayMentors = [
-    {
-        name: "Fernanda",
-        lastName: "Palacios"
-    },
-    {
-        name: "Alfred",
-        lastName: "Altamirano"
-    },
-]
+// let arrayMentors = [
+//     {
+//         name: "Fernanda",
+//         lastName: "Palacios"
+//     },
+//     {
+//         name: "Alfred",
+//         lastName: "Altamirano"
+//     },
+// ]
 
-arrayMentors.forEach((mentor, index)=> {
-    console.log(mentor.name)
-    console.log(index)
-})
+// arrayMentors.forEach((mentor, index)=> {
+//     console.log(mentor.name)
+//     console.log(index)
+// })
 
 
 // Ejercicio
 
-let mentorArray = [
+let mentorsArray = [
     {
         name: "Angel Resendiz",
         scores: [
@@ -255,15 +255,15 @@ let mentorArray = [
         name: "Fernanda Palacios",
         scores: [
             {
-                signature:  "HTML",
-                score: 10
-            },
-            {
                 signature:  "CSS",
                 score: 9
             },
             {
                 signature:  "JS",
+                score: 10
+            },
+            {
+                signature:  "HTML",
                 score: 10
             },
         ]
@@ -283,6 +283,119 @@ let mentorArray = [
 
 */
 
+/*
+- Obtener el score promedio de cada materia (HTML, CSS, JS)
+
+html
+css
+js
+*/
 
 
+const getAverageAsignatures = arrayMentors => {
+    let htmlAccum = 0
+    let cssAccum = 0
+    let jsAccum = 0
+    let mentorsQty = arrayMentors.length
+    arrayMentors.forEach((mentorObject, index) => {
+        // console.log(mentorObject)
+        mentorObject.scores.forEach((scoreMentor)=> {
+            // console.log(scoreMentor)
+            let signature = scoreMentor.signature
+            let score = scoreMentor.score
 
+            switch(signature) {
+                case "HTML": 
+                    htmlAccum += score
+                    break;
+                case "CSS":
+                    cssAccum += score
+                    break;
+                case "JS":
+                    jsAccum += score
+                    break;
+                default:
+                    console.log("Asignatura no encontrada")
+            }
+
+        })
+    })
+
+    let avgHtml = htmlAccum / mentorsQty
+    let avgCss = cssAccum / mentorsQty
+    let avgJs = jsAccum / mentorsQty
+
+    let objectAvg = { avgHtml, avgCss, avgJs}
+    return objectAvg
+}
+
+//invocar
+let avgSignatures = getAverageAsignatures(mentorsArray)
+console.log(avgSignatures)
+
+/*
+- Obtener el promedio indivual de cada mentor
+*/
+
+const getAvgForMentor = scoresArray => {
+    let accumMentor = 0
+    let scoresQty = scoresArray.length
+    scoresArray.forEach((scoreMentor)=> {
+        let valueScore = scoreMentor.score
+        accumMentor += valueScore
+    })
+    let avgMentor = accumMentor / scoresQty
+    return avgMentor
+}
+
+/*
+arrayAverage = [
+    { 
+        name: "",
+        average: 9.0
+    }
+]
+
+*/
+
+
+const getAvgMentors = arrayMentors => {
+    let arrayAverageMentors = []
+    arrayMentors.forEach((mentor)=> {
+        let avg = getAvgForMentor(mentor.scores)
+        let objectMentor = { 
+            name: mentor.name, 
+            average: avg
+        }
+        arrayAverageMentors.push(objectMentor)
+    })
+    return arrayAverageMentors
+}
+
+let avgMentors = getAvgMentors(mentorsArray)
+
+console.log(avgMentors)
+
+/*
+- Crear un array de string con la siguiente forma:
+  "Mi nombre es {name} y mi promedio es {promedio}"
+*/
+
+let arrayStringsMentors = []
+
+avgMentors.forEach((mentor)=> {
+    // destructuring
+    let { name, average,  } = mentor
+    arrayStringsMentors.push(`Mi nombre es ${name} y mi promedio es ${average}`)
+})
+
+console.log(arrayStringsMentors)
+
+// - Obtener la lista de mentores cuyo promedio sea mayor a 9.5
+
+let mentorsTop = []
+
+avgMentors.forEach((mentor)=> {
+    mentor.average >= 9.5 ? mentorsTop.push(mentor) : null
+})
+console.log(mentorsTop)
