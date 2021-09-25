b244dda69fbe1febcd3ae80cb832487c2e7083ce
Fernanda Palacios <fernandapalacios.dev@gmail.com>
2021-09-24 20:10:05 -0500

    [UPDATE] table build

--- 14-intro-dom/js/main.js
+++ 14-intro-dom/js/main.js
@@ -54,3 +54,88 @@ document.body.appendChild(footer)
 
 // 
 
+let mentorsArray = [
+    {
+        name: "Fernanda",
+        mail: "fernanda@kodemia.mx",
+        phone: "9671686557",
+        avg: 9
+    },
+    {
+        name: "Alfred",
+        mail: "alfred@kodemia.mx",
+        phone: "5523452345", 
+        avg: 10
+    },
+    {
+        name: "Angel",
+        mail: "angle@kodemia.mx",
+        phone: "5541686557", 
+        avg: 10
+    }
+]
+
+const getAgvGeneral = (arrayMentors) => {
+    return arrayMentors.reduce((accum,mentor)=> accum + mentor.avg, 0) / arrayMentors.length
+}
+
+const printTable = () => {
+    let headersTable = ["Nombre", "E-mail", "Phone"]
+    let table = document.createElement("table")
+    let thead = document.createElement("thead")
+    let headerRow = document.createElement("tr")
+
+    headersTable.forEach(title => {
+        let headTd = document.createElement("td")
+        let textHead = document.createTextNode(title)
+        headTd.appendChild(textHead)
+        headerRow.appendChild(headTd)
+    })
+    thead.appendChild(headerRow)
+    table.appendChild(thead)
+    
+
+    let tbody = document.createElement("tbody")
+
+
+    mentorsArray.forEach(mentor => {
+        // let trMentor = document.createElement("tr")
+        // let nameTd = document.createElement("td")
+        // let mailTd = document.createElement("td")
+        // let phoneTd = document.createElement("td")
+
+        // let nameText = document.createTextNode(mentor.name)
+        // let mailText = document.createTextNode(mentor.mail)
+        // let phoneText = document.createTextNode(mentor.phone)
+
+        // nameTd.appendChild(nameText)
+        // mailTd.appendChild(mailText)
+        // phoneTd.appendChild(phoneText)
+
+        // trMentor.appendChild(nameTd)
+        // trMentor.appendChild(mailTd)
+        // trMentor.appendChild(phoneTd)
+
+        // tbody.appendChild(trMentor)
+        tbody.innerHTML += `
+            <tr>
+                <td>${mentor.name}</td>
+                <td>${mentor.mail}</td>
+                <td>${mentor.phone}</td>
+            </tr>
+        `
+    })
+    table.appendChild(tbody)
+    // console.log(table.innerHTML)
+    // let saludo = "Hola Koders!!"
+    // table.innerHTML = `<p>${saludo}</p>`
+    // table.textContent = "<p>Hola Koders</p>"
+    document.body.appendChild(table)
+
+    let phrase = document.createElement("p")
+    let textAvg = document.createTextNode(`El promedio general es: ${getAgvGeneral(mentorsArray).toFixed(1)}`)
+
+    phrase.appendChild(textAvg)
+
+    document.body.appendChild(phrase)
+}
