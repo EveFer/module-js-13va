7c29b95fb95a86ac1e4b7853bbae1410b60e231f
Fernanda Palacios <fernandapalacios.dev@gmail.com>
2021-09-27 21:37:04 -0500

    [UPDATE] created printTable function

--- 16-listeners/js/main.js
+++ 16-listeners/js/main.js
@@ -36,3 +36,59 @@ buttons.forEach((btn) => {
         counter.textContent = number
     })
 })
+
+let kodersArray = [
+    {
+        name: "Emi",
+        lastName:  "de León"
+    },
+    {
+        name: "Clau", 
+        lastName: "Rodriguez"
+    },
+    {
+        name: "Fanny", 
+        lastName: "Alvarez"
+    }
+]
+
+const createNode = (typeElement, text) => {
+    let node = document.createElement(typeElement)
+    node.textContent = text
+    // let textNode = document.createTextNode(text)
+    // node.appendChild(textNode)
+    return node
+}
+
+const printTable = () => {
+    let tBody = document.getElementById("list-koders")
+    kodersArray.forEach((koder, index) => {
+        let {name, lastName} = koder
+        let tr = document.createElement("tr")
+
+        let tdIndex = createNode("td", index + 1)
+        let tdName = createNode("td", name)
+        let tdLastName = createNode("td", lastName)
+        let tdButton = document.createElement("td")
+
+        let button = createNode("button", "Eliminar")
+        button.classList.add("btn", "btn-danger")
+
+        button.addEventListener("click", ()=> {
+            console.log("Eliminando... jeje")
+            // Eliminar del array
+        })
+
+        tdButton.appendChild(button)
+
+        tr.appendChild(tdIndex)
+        tr.appendChild(tdName)
+        tr.appendChild(tdLastName)
+        tr.appendChild(tdButton)
+
+        tBody.appendChild(tr)
+    })
+}
+
+printTable()
+
